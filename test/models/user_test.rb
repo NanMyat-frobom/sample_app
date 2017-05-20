aa48260f2aecbfb4e8e963f8b65db6424ee5e079
NanMyat <nanmyat12@gmail.com>
2017-05-20 10:28:49 +0630

    Add account activation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -67,6 +67,6 @@ test "password should be present (nonblank)" do
   end
  
   test "authenticated? should return false for a user with nil digest" do
-   assert_not @user.authenticated?('')
+  assert_not @user.authenticated?(:remember, '')
   end
 end
